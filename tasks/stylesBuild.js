886d031a2b8ac17235e059c9f034936735a92a54
Olivier Chavarin <memoire1@gmail.com>
2022-09-26 04:58:45 +0200

    Demo: toggle switch updated

--- tasks/stylesBuild.js
+++ tasks/stylesBuild.js
@@ -1,10 +1,12 @@
 import { readdirSync, writeFileSync } from 'fs'
 import stylus from 'stylus'
 
-const str = require('fs').readFileSync('./styles/development/main.styl', 'utf8')
+const date = new Date().toISOString().split('T')
+const version = date[0].replace(/-/g, '') + Math.round(date[1].split('.')[0].replace(/:/g, '') / 500)
+const str = readdirSync('./styles/development/main.styl', 'utf8')
 
 stylus(str)
-  .set('filename', 'main.css')
+  .set('filename', './styles/' + version + 'main.css')
   .render(function(err, css) {
     // logic
   })
