03942166031ab3d3af8a0da01027a3285adfb716
Olivier Chavarin <memoire1@gmail.com>
2022-10-30 09:37:32 +0100

    Videos updated

--- scripts/youtubeVideo.js
+++ scripts/youtubeVideo.js
@@ -1,54 +1,47 @@
 'use strict'
 
-const youtubeVideo = (() => {
-  document.querySelectorAll('.video-youtube').forEach(e => {
-    const id = e.dataset.id
-    //const json = `https://youtube.com/oembed?url=http://www.youtube.com/watch?v=${id}&format=json`
-    const button = document.createElement('button')
-    button.classList.add('thumbnail-youtube')
-    button.style.backgroundImage = `url("https://img.youtube.com/vi/${id}/maxresdefault.jpg")`
-    button.innerHTML = `<svg role="img" focusable="false"><use href="/sprites/utils.svg#video-play"></use></svg>`
-    e.appendChild(button)
-    e.addEventListener('click', () => {
-      const iframe = document.createElement('iframe')
-      iframe.src = `https://www.youtube.com/embed/${id}?feature=oembed`
-      iframe.title = 'test'
-      iframe.setAttribute('allowFullScreen', '') // @todo 'allow: fullscreen' n'est pas encore supporté
-      iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
-      //iframe.dataset.title = 'test'
-      //iframe.dataset.author = 'Georges'
-      e.appendChild(iframe)
-    })
-  })
-})()
-
+// @documentation :
+// @see https://www.thewebtaylor.com/articles/how-to-get-a-youtube-videos-thumbnail-image-in-high-quality
+// API JSON:
 // https://youtube.com/oembed?url=http://www.youtube.com/watch?v=SMYuFq84E9Y&format=json
-/*
+
 const youtubeVideo = (() => {
+
   document.querySelectorAll('.video-youtube').forEach(e => {
-    const url = `https://youtube.com/oembed?url=http://www.youtube.com/watch?v=${e.dataset.id}&format=json`
+
+    let error = false
+    const id = e.dataset.id
+    const url = `https://youtube.com/oembed?url=http://www.youtube.com/watch?v=${id}` // Par défaut : `&format=json` ; alternative : `&format=xml`
+
     fetch(url)
-    .then(res => {
-      if (res.statusText === 'OK') {
-        return res.text();
-      }
-      throw new Error(res.statusText)
-    })
-    .then(data => {
-      output.innerHTML = data
-    })
-    .catch(error => console.log(error))
-    e.addEventListener('click', () => {
-      //e.innerHTML = e.innerHTML.replace(/^..*$/, '')
-      const iframe = document.createElement('iframe')
-      iframe.src = `https://www.youtube.com/embed/${e.dataset.id}?feature=oembed`
-      iframe.title = 'test'
-      iframe.setAttribute('allowFullScreen', '') // @todo 'allow: fullscreen' n'est pas encore supporté
-      iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
-      iframe.dataset.title = 'test'
-      iframe.dataset.author = 'Georges'
-      e.appendChild(iframe)
-    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        const button = document.createElement('button')
+        button.classList.add('thumbnail-youtube')
+        button.style.backgroundImage = `url(${data.thumbnail_url})` // @note Le top qualité, mais pas toujours disponible : `https://img.youtube.com/vi/${id}/maxresdefault.jpg`
+        button.innerHTML = `<svg role="img" focusable="false"><use href="/sprites/utils.svg#video-play"></use></svg><p>${data.title}</p>`
+        e.appendChild(button)
+    
+        e.addEventListener('click', () => {
+          button.remove()
+          const iframe = document.createElement('iframe')
+          iframe.src = `https://www.youtube.com/embed/${id}?feature=oembed&autoplay=1&enablejsapi=1`
+          iframe.title = data.title
+          iframe.setAttribute('allowFullScreen', '') // @todo 'allow: fullscreen' n'est pas encore supporté
+          //iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture')
+          e.appendChild(iframe)
+        })
+      })
+      .catch(error => {
+        error = true
+        const div = document.createElement('div')
+        div.classList.add('thumbnail-youtube')
+        div.innerHTML = `<p>Erreur : Cette vidéo n'existe pas :(</p>`
+        e.appendChild(div)
+        console.error('There was an error!', error)
+      })
+
   })
+
 })()
-*/
