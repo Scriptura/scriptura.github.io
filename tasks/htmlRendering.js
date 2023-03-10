8b59952a9fca00587869f157accf721c10640107
Olivier Chavarin <memoire1@gmail.com>
2023-03-10 21:53:51 +0100

    Revision of the folder hierarchy

--- tasks/htmlRendering.js
+++ tasks/htmlRendering.js
@@ -1,23 +1,15 @@
-import { readdirSync, writeFileSync } from 'fs'
+import { readdirSync, writeFileSync, readFileSync } from 'fs'
 import { renderFile } from 'pug'
+
+//const sprites = readFileSync('sprites/util.svg')
 
 writeFileSync('index.html', renderFile('views/index.pug'))
 writeFileSync('404.html', renderFile('views/404.pug'))
-writeFileSync('page/login.html', renderFile('views/login.pug'))
-writeFileSync('page/register.html', renderFile('views/register.pug'))
-writeFileSync('page/styleGuide.html', renderFile('views/styleGuide.pug'))
-writeFileSync('page/templates.html', renderFile('views/templates.pug'))
-writeFileSync('page/person.html', renderFile('views/person.pug'))
-writeFileSync('page/place.html', renderFile('views/place.pug'))
-writeFileSync('page/article.html', renderFile('views/article.pug'))
-writeFileSync('page/forum.html', renderFile('views/forum.pug'))
-writeFileSync('page/topic.html', renderFile('views/topic.pug'))
-writeFileSync('page/imageGallery.html', renderFile('views/imageGallery.pug'))
 
-const files = readdirSync('./views/includes/demos/pages/')
+const files = readdirSync('views/pages/')
 
 for (let file of files) {
   file = file.toString().slice(0, -4)
-  writeFileSync(`page/${file}.html`, renderFile(`views/includes/demos/pages/${file}.pug`))
+  writeFileSync(`page/${file}.html`, renderFile(`views/pages/${file}.pug`))
   //console.log('Created ' + file + '.html')
 }
