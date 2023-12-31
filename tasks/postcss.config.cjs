58236a3f851f17c11b8fe8fdbe7101bd1982d005
Olivier Chavarin <memoire1@gmail.com>
2023-12-31 21:19:16 +0100

    PostCSS configured

---
+++ tasks/postcss.config.cjs
@@ -0,0 +1,17 @@
+// @see https://www.freecodecamp.org/news/what-is-postcss/
+
+module.exports = {
+  map: { inline: false },
+  plugins: [
+    require('postcss-import'),
+    require('postcss-advanced-variables'),
+    require('postcss-calc'),
+    require('postcss-preset-env')({
+      stage: 4,
+      features: {
+        'nesting-rules': true
+      }
+    }),
+    process.env === 'compressed' ? require('postcss-minify') : ''
+  ],
+}
