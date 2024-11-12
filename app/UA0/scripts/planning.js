a0bd5fb1ff76c7ddfdab172615762f57b3cc7cfa
Olivier Chavarin <memoire1@gmail.com>
2024-11-12 11:18:54 +0100

    App : add icons

--- app/UA0/scripts/planning.js
+++ app/UA0/scripts/planning.js
@@ -620,6 +620,7 @@ document.addEventListener('DOMContentLoaded', () => {
   generateButton.addEventListener('click', () => {
     CalendarManager.generateSchedule(startDateInput.value, getSelectedPattern())
     StorageManager.loadSchedule(calendarDiv)
+    alert(`Le planning a bien été créé.`)
   })
 
   if (resetButton) {
