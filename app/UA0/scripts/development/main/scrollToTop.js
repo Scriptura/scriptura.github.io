a8fa9ad57b32ff289efb92070ffda08c3dbce0c5
Olivier Chavarin <memoire1@gmail.com>
2024-12-05 02:59:14 +0100

    Update scrollToTop.js

--- app/UA0/scripts/development/main/scrollToTop.js
+++ app/UA0/scripts/development/main/scrollToTop.js
@@ -1,23 +1,16 @@
 function scrollToTop() {
   const footer = document.querySelector('.footer')
   const button = document.createElement('button')
+  const icon = `<svg focusable="false"><use href="./sprites/util.svg#arrow_upward"></use></svg>`
 
   button.type = 'button'
-  button.classList.add('scroll-top')
+  button.classList.add('scroll-top', 'fade-out')
   button.setAttribute('aria-label', 'Scroll to top')
-
-  //injectSvgSprite(button, 'arrow-up')
-  const icon = `<svg focusable="false"><use href="./sprites/util.svg#arrow_upward"></use></svg>`
   button.insertAdjacentHTML('beforeEnd', icon)
-
   footer.appendChild(button)
 
-  const item = document.querySelector('.scroll-top')
-
-  button.classList.add('fade-out')
-
   function position() {
-    const yy = window.innerHeight / 2 // @note Scroll sur la demi-hauteur d'une fenêtre avant apparition de la flèche.
+    const yy = window.innerHeight / 2
     let y = window.scrollY
     if (y > yy) {
       button.classList.remove('fade-out')
